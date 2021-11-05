6ca385ee12772671e4ea3f8d011d72c408113e62
Ulas <ozulashe@gmail.com>
2021-11-05 10:12:20 +0300

    For preview

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,9 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hi, I am Ulaş Öztürk.</p>
         <p>
+          Checkout my <a href="https://github.com/Ulashe">GitHub</a>GitHub profile.
+        </p>
+        <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
